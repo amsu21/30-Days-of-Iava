016f73013936f73ef5ded23daa470136f45407df
amsu21 <aswb4@hotmail.com>
2024-07-18 18:41:30 -0400

    30-Days-Java

---
+++ src/01Day01/OUTPUT_STATEMENT/first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("HELLO"); // FIRST STATEMENT
+        // if print is occupied with ln
+        // it means that it will automatically switch or change lines
+        // after first statement and print second statement
+        System.out.println("WORLD");
+    }
+}
