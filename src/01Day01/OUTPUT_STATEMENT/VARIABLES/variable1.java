c8a962613f5d71aa4816de6ba649e2384c02ff58
amsu21 <aswb4@hotmail.com>
2024-07-18 18:54:32 -0400

    variables folder

---
+++ src/01Day01/OUTPUT_STATEMENT/VARIABLES/variable1.java
@@ -0,0 +1,13 @@
+package VARIABLES;
+
+public class variable1 {
+    public static void main(String[] args) {
+        int xyz = 21;
+        String abc = "HELLO WORLD";
+        long A = 2;
+
+        System.out.println(A);
+        System.out.println(abc);
+        System.out.println(xyz);
+    }
+}
---
+++ src/01Day01/OUTPUT_STATEMENT/VARIABLES/variable2.java
@@ -0,0 +1,23 @@
+package VARIABLES;
+
+public class variable2 {
+    public static void main(String[] args) {
+        // MULTIPLE VARS OF THE SAME TYPE
+        int a = 21, b = 21, c = 3, j = 89;
+        String firstname = "Amsu", lastname = "Warner";
+
+        int sum = a + b;
+
+        int mul = c * j;
+
+        int minus = j - a;
+
+        int div = b / c;
+
+        System.out.println("SUM IS: " + sum);
+        System.out.println("PRODUCT IS: " + mul);
+        System.out.println("DIFFERENCE IS: " + minus);
+        System.out.println("COEFFICENT IS: " + div);
+        System.out.println(firstname + " " + lastname);
+    }
+}
